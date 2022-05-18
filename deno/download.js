3c7146a66454c710aab279636380f92f1b335c73
Taisuke Fukuno <fukuno@jig.jp>
2022-05-19 08:17:15 +0900

    first commit

---
+++ deno/download.js
@@ -0,0 +1,13 @@
+import { CSV } from "https://js.sabae.cc/CSV.js";
+
+const url = "https://api.evfcv.awstemp.toyota.jp/?method=getHydrogenStationInfo&lat=141.936598&lon=45.523156&result=json&collect_count=0";
+const data = await (await fetch(url)).json();
+const list = data.resultData.stationList.map(d => {
+  for (const name in d.info) {
+    d[name] = d.info[name];
+  }
+  delete d.info;
+  return d;
+});
+await Deno.writeTextFile("../data/hydrogen-station-info.json", JSON.stringify(data, null, 2));
+await Deno.writeTextFile("../data/hydrogen-station-info.csv", CSV.stringify(list));
